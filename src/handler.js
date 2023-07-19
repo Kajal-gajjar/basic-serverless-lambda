db58c570deeeaeef9b362b1ba4d1e6fe628ba874
Kajal-gajjar <kngajjar123@gmail.com>
2023-07-19 12:40:37 +0530

    Update handler.js

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,22 @@
-'use strict';
+import { CodePipeline } from 'aws-sdk';
+const pipeline = new CodePipeline();
 
-module.exports.generateRandomNumber = async (event) => {
- const randomNumber = parseInt(Math.random()*100);
- console.log("The random generated integer is: ", randomNumber);
- return randomNumber;
-};
+export async function generateRandomNumber(event) {
+  const randomNumber = parseInt(Math.random() * 100);
+  console.log('The random generated integer is:', randomNumber);
+
+  // Call PutJobSuccessResult to notify CodePipeline of successful execution
+  const params = {
+    jobId: event['CodePipeline.job']['id'],
+  };
+
+  try {
+    // PutJobSuccessResult does not return any data, so you don't need to store the response
+    await pipeline.putJobSuccessResult(params).promise();
+  } catch (error) {
+    console.error('Error calling PutJobSuccessResult:', error);
+    throw error;
+  }
+
+  return randomNumber;
+}
